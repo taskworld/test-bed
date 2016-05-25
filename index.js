91de535aefb080686c43453154f5171cb6dee4e4
Thai Pangsakulyanont @ Taskworld <thai.p@taskworld.com>
2016-05-25 16:27:44 +0700

    Message

--- index.js
+++ index.js
@@ -8,5 +8,12 @@ const io = require('socket.io')(server)
 
 app.use(express.static(__dirname + '/static'))
 server.listen(9011, function () {
-  console.log('Listen: ', this.address())
+  console.log('')
+  console.log('++====================================================++')
+  console.log('|| test-bed is now running                            ||')
+  console.log('||                                                    ||')
+  console.log('|| Please open http://localhost:9011/ in your browser ||')
+  console.log('|| in order to run tests.                             ||')
+  console.log('++====================================================++')
+  console.log('')
 })
