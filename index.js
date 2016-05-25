0f362fbbe0fcb75c6084d159b2bd9cd45a9f3fe4
Thai Pangsakulyanont @ Taskworld <thai.p@taskworld.com>
2016-05-25 16:01:43 +0700

    Hackathon version commit

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+app.use(express.static(__dirname + '/static'))
+server.listen(9011)
---
+++ static/testbed.js
@@ -0,0 +1,97 @@
+/* global mocha */
+
+window.TestBed = (function () {
+  var _files
+  var _cache
+  var status = document.querySelector('#testbed-status')
+
+  var maybeFrame = (function () {
+    var _last = 0
+    return function (f) {
+      if (Date.now() - _last < 16) {
+        f()
+      } else {
+        window.requestAnimationFrame(function () {
+          _last = Date.now()
+          f()
+        })
+      }
+    }
+  })()
+
+  function updateStatus (content) {
+    status.textContent = content
+  }
+
+  function saveCache (files) {
+    var cache = { }
+    files.forEach(function (file) {
+      cache[file.name] = file.fn
+    })
+    _cache = cache
+  }
+
+  function go (files) {
+    if (_cache) {
+      var changedFiles = files.filter(function (file) {
+        return _cache[file.name] !== file.fn
+      })
+      updateStatus('received ' + changedFiles.length + ' changed files.')
+      rerun(changedFiles)
+    } else {
+      saveCache(files)
+      updateStatus('received ' + files.length + ' files.')
+      var filteredFiles = filterFiles(files)
+      requireFile(filteredFiles, 0, function () {
+        mocha.run()
+      })
+    }
+  }
+
+  function rerun (files) {
+    var nextFiles = (files.map(function (file) {
+      return file.name
+    }).join(';;'))
+    window.sessionStorage.testFiles = nextFiles
+    location.reload()
+  }
+
+  function filterFiles (files) {
+    var filter = window.sessionStorage.testFiles || ''
+    if (!filter) return files
+    var applicable = filter.split(';;')
+    return files.filter(function (file) {
+      return applicable.indexOf(file.name) >= 0
+    })
+  }
+
+  function requireFile (files, index, onFinish) {
+    var current = files[index]
+    maybeFrame(function () {
+      if (current) {
+        updateStatus('is requiring ' + current.name + ' (' + (index + 1) + '/' + files.length + ')...')
+        current.fn()
+        maybeFrame(function () {
+          requireFile(files, index + 1, onFinish)
+        })
+      } else {
+        updateStatus('finished requiring files.')
+        onFinish()
+      }
+    })
+  }
+
+  return {
+    register: function (receiveFiles) {
+      if (_files) throw new Error('register in register!!')
+      _files = [ ]
+      receiveFiles()
+      var files = _files
+      _files = null
+      go(files)
+    },
+    handleFile: function (name, fn) {
+      _files.push({ name: name, fn: fn })
+    }
+  }
+})()
