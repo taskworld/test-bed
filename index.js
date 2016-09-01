7e4eb7a08411e9d55837750723b2a271a84f3ab2
Lukas Taegert <lukas.taegert@tngtech.com>
2016-09-01 18:37:50 +0200

    feat(CLI): Add the ability to pass the webpack config file as command line parameter to enable the use of different configurations within the same project

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
 'use strict'
 
-const config = require(require('path').resolve(process.cwd(), 'webpack.config.test-bed.js'))
+const relativeConfigPath = require('yargs')
+  .option('c', {
+    alias: 'config',
+    demand: false,
+    default: 'webpack.config.test-bed.js',
+    describe: 'Specify the configuration file',
+    type: 'string'
+  })
+  .nargs('config', 1)
+  .help()
+  .argv.config;
+
+const config = require(require('path').resolve(process.cwd(), relativeConfigPath))
 const server = require('./createServer')(config)
 
 server.listen(9011, function () {
